6ef4f7aa9f6956304f7b067b0ae499b6205be090
byemidev <97583628+byemidev@users.noreply.github.com>
2024-08-08 09:23:01 +0200

    Update index.js
    changing text for index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Home = () => {
           bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
           css={{ backdropFilter: "blur(10px)" }}
         >
-          Hello, I&apos;m an indie app developer based in Japan!
+          Hello, I&apos;m an indie app developer based in Spain!
         </Box>
         <Wrap>
           <WrapItem>
